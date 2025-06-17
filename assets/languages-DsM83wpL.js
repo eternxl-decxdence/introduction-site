5d399067c2a819e4daf26100d578c67380f17c5a
KanaAishiro <sonicsoliar@gmail.com>
2025-06-17 18:21:55 +0200

    Updates

--- assets/languages-DsM83wpL.js
+++
@@ -1,1 +0,0 @@
-const e=[{sectionName:"Языки",sectionData:[{langName:"Украинский",level:5},{langName:"Русский",level:5},{langName:"Польский",level:4},{langName:"Английский",level:4}]}];export{e as default};
